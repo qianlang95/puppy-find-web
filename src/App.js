21565e254c7aa04fb31f653e277a4dd5edf6f1c8
menth0s <marslite@pm.me>
2023-11-11 10:46:06 -0500

    Started implementing Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {HashRouter} from "react-router-dom";
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Home from './Home';
+
+
+
+
+
+function App(){
+
+
+  return(
+    <HashRouter>
+      <div>
+        {/* <h1>hello</h1> */}
+        <Routes>
+          {/* <Route path='/'  /> */}
+
+          <Route  path='/' element={<Home/>} />
+
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
 export default App;
+
+
+
+
+// function App() {
+//   return (
+
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+
+// }
---
+++ src/Home/index.js
@@ -0,0 +1,31 @@
+
+
+function Home(){
+
+    return(
+        <div>
+
+            <h2>Testing home</h2>
+
+            <nav className="navbar">
+                <ul>
+                    <li>lol</li>
+                    <li>lol</li>
+                    <li>lol</li>
+                </ul>
+            </nav>
+
+            <footer>
+                <ul>
+                    <li>lol</li>
+                    <li>lol</li>
+                    <li>lol</li>
+                </ul>
+            </footer>
+
+
+        </div>
+    );
+}
+
+export default Home;
