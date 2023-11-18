3d61b67486f1a1d5256589239f61873dd9ca194d
menth0s <marslite@pm.me>
2023-11-18 07:29:48 -0500

    Imported new font for Puppy Finder

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,7 +2,7 @@
 import "./index.css"
 import Navbar from "./Navbar/navbar"
 import FooterNav from "./Footer"
-import "./misc/sl_011023_55240_03.jpg"
+import  pupImage from "./misc/english-springer-spaniel-dog-puppy-artistic-style-painting-drawing-cartoon-style-illustration-no-background-perfect-for-print-on-demand-merchandise-ai-generative-png-2610760852.png"
 
 function Home(){
 
@@ -28,6 +28,12 @@ function Home(){
                     <p class="card-text">On PuppyFinder you can find, search and discover puppies that needs a home.</p>
                  </div>
              </div>  
+
+             <div class="alert alert-warning alert_mv" role="alert">
+                To list or start a petition for adopting a puppy you must be registered <a href="#" class="alert-link">Click here to register</a>.
+            </div>
+
+
             {/* //The puppy cards will be added here */}
 
             <div className="container-fluid flex-wrap moved">
@@ -37,58 +43,29 @@ function Home(){
                 {/* <div className="col white_spacing_up"> */}
 
                 <div className="card mv_up" style={{"width": "18rem"}}>
-                    <img src="sl_011023_55240_03.jpg" className="card-img-top" alt="..." />
+                    <img src={pupImage} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item">An item</li>
-                        <li className="list-group-item">A second item</li>
-                        <li className="list-group-item">A third item</li>
+                        <li className="list-group-item">Breed: Spaniel</li>
+                        <li className="list-group-item">Age: 1 month</li>
+                        <li className="list-group-item">Location: San Jose, CA</li>
+                        {/* <li className="list-group-item">A third item</li> */}
                     </ul>
-                    <div className="card-body">
-                        <a href="#" className="card-link">Card link</a>
-                        <a href="#" className="card-link">Another link</a>
-                    </div>
-                </div>
 
-                <div className="card mv_up" style={{"width": "18rem"}}>
-                    <img src="sl_011023_55240_03.jpg" className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">An item</li>
-                        <li className="list-group-item">A second item</li>
-                        <li className="list-group-item">A third item</li>
-                    </ul>
-                    <div className="card-body">
+                    {/* <div className="card-body">
                         <a href="#" className="card-link">Card link</a>
                         <a href="#" className="card-link">Another link</a>
-                    </div>
+                    </div> */}
+
                 </div>
 
 
-                <div className="card mv_up" style={{"width": "18rem"}}>
-                    <img src="sl_011023_55240_03.jpg" className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">An item</li>
-                        <li className="list-group-item">A second item</li>
-                        <li className="list-group-item">A third item</li>
-                    </ul>
-                    <div className="card-body">
-                        <a href="#" className="card-link">Card link</a>
-                        <a href="#" className="card-link">Another link</a>
-                    </div>
-                </div>
 
-                {/* </div> */}
+
+      
 
 
 
@@ -109,7 +86,7 @@ function Home(){
 
 
             <div>
-                <h2>test</h2>
+                {/* <h2>test</h2> */}
              </div>
 
 
