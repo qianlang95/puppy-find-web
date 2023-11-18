5b365099fae1c5a15acc59fe9dac1b967bd57183
menth0s <marslite@pm.me>
2023-11-18 06:41:16 -0500

    Implementing pup cards

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,6 +2,7 @@
 import "./index.css"
 import Navbar from "./Navbar/navbar"
 import FooterNav from "./Footer"
+import "./misc/sl_011023_55240_03.jpg"
 
 function Home(){
 
@@ -28,9 +29,71 @@ function Home(){
                  </div>
              </div>  
             {/* //The puppy cards will be added here */}
-            <div>
-                <h2>test</h2>
-             </div>
+
+            <div className="container-fluid flex-wrap moved">
+
+
+                <div className="row spacing">
+                {/* <div className="col white_spacing_up"> */}
+
+                <div className="card mv_up" style={{"width": "18rem"}}>
+                    <img src="sl_011023_55240_03.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">An item</li>
+                        <li className="list-group-item">A second item</li>
+                        <li className="list-group-item">A third item</li>
+                    </ul>
+                    <div className="card-body">
+                        <a href="#" className="card-link">Card link</a>
+                        <a href="#" className="card-link">Another link</a>
+                    </div>
+                </div>
+
+                <div className="card mv_up" style={{"width": "18rem"}}>
+                    <img src="sl_011023_55240_03.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">An item</li>
+                        <li className="list-group-item">A second item</li>
+                        <li className="list-group-item">A third item</li>
+                    </ul>
+                    <div className="card-body">
+                        <a href="#" className="card-link">Card link</a>
+                        <a href="#" className="card-link">Another link</a>
+                    </div>
+                </div>
+
+
+                <div className="card mv_up" style={{"width": "18rem"}}>
+                    <img src="sl_011023_55240_03.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">An item</li>
+                        <li className="list-group-item">A second item</li>
+                        <li className="list-group-item">A third item</li>
+                    </ul>
+                    <div className="card-body">
+                        <a href="#" className="card-link">Card link</a>
+                        <a href="#" className="card-link">Another link</a>
+                    </div>
+                </div>
+
+                {/* </div> */}
+
+
+
+
+                </div>
 
 
 
@@ -39,8 +102,18 @@ function Home(){
 
             </div>
 
-    
 
+
+
+
+
+
+            <div>
+                <h2>test</h2>
+             </div>
+
+
+            </div>
 
             <FooterNav/>
 
