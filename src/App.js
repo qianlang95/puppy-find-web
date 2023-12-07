23af3a9965b4ae9d61894030febbef5b61d1a611
Silva liao <liao.suj@northeastern.edugit config --global user.name silvaliaoo>
2023-12-06 20:00:47 -0800

    Add src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Signin from "./users/signin";
+import Account from "./users/account";
+import UserTable from "./users/table";
+import Signup from "./users/signup";
+import UserDetails from "./users/details";
+import CurrentUser from "./users/currentUser";
+import store from "./users/store";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Provider } from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <CurrentUser>
+          <div>
+            <Routes>
+              <Route path="/"         element={<Navigate to="/login"/>}/>
+              <Route path="/login" element={<Signin />} />
+              <Route path="/register" element={<Signup />} />
+              <Route path="/account" element={<Account />} />
+              <Route path="/admin/users" element={<UserTable />} />
+              <Route path="/account/:id" element={<Account />} />
+              <Route path="/profile/:id" element={<UserDetails />} />
+              
+
+            </Routes>
+          </div>
+        </CurrentUser>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
